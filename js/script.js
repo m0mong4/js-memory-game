2d1619b5a62e90a7db392003ca29f64bb4e4842d
Iria DE MEYER <demeyer251@unicaen.fr>
2026-09-18 10:06:21 +0200

    ajout de la fonction shuffle

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ for (let i = 0; i < 8; i++) {
 console.log(images);
 
 let cards = [...images, ...images];
+
+function shuffle(array){
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+cards = shuffle(cards);
